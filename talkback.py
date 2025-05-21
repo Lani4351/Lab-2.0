bc828421d184b3137e6340de8ede5444119bc8c2
Lani4351 <natenunez967@gmail.com>
2025-05-20 21:20:02 -0400

    Add files via upload

--- talkback.py
+++ talkback.py
@@ -1,8 +1,8 @@
 # i want this program to ask the user for
 #their name "What's your name?" and then,
 # i want the program to say "Hi, NAME"
-# then i want it to ask the user's age
-# and make a comment on it
+# then i want it to ask the user's birth year and subtract it from the current year
+# to calulate the user's age and respond with how old they are
 
 # step 1, ask the user what their name is
 print("What is your name?")
@@ -12,8 +12,10 @@ user_name = input()
 # step 3: say hi back using the user's name
 print("Hi, " + user_name , ".")
 # step 4: ask user for their age
-print("How old are you? ")
-# step 5: allow user to enter their age
-user_age = input()
-# step 6: add a response
-print("Wow! " + user_age , "is old!")
+print("What year were you born?")
+# step 5: allow user to enter their birth year
+user_birthyear = input()
+# step 6: subtract birth year from current year
+# print final answer with response to user's age
+print("Wow! So that means you are about "
+      + str(2025 - int(user_birthyear)) + " years old!")
